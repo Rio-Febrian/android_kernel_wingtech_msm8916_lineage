545dabdff00b1905ce2f53e604888e4769bcc63b
iamsubhranil <subhranilmukherjee190@gmail.com>
2017-01-07 18:10:28 +0530

    Added my personal scripts

---
+++ build_starter.sh
@@ -0,0 +1,39 @@
+echo "Declaring directories.."
+KERNEL_DIR=$(pwd)
+cd ..
+MAIN_DIR=$(pwd)
+DTBTOOL_DIR=$MAIN_DIR/mkbootimg_tools
+echo "Switching to kernel directory.."
+cd $KERNEL_DIR
+echo "Exporting arch, subarch and compiler directories.."
+export USE_CCACHE=1
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=$MAIN_DIR/arm-eabi-4.8/bin/arm-eabi-
+read -p "Do you want to rebuild all? " rebuild
+case $rebuild in
+	[Yy]* ) 
+	echo "Cleaning previously compiled files..";
+	make clean && make mrproper;
+esac
+read -p "Do you want to overwrite present configuration with the default one? " choice
+case $choice in
+	[Yy]* )
+	echo "Creating default configuration..";
+	make wt88047_defconfig;
+esac
+read -p "How many threads do you want to use for compiling the kernel? " threads
+echo "Compiling kernel.."
+make -j$threads
+echo "Switching to dtbTool directory.."
+cd $DTBTOOL_DIR
+echo "Executing dtbToolCM.."
+./dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+echo "Kernel compilation complete!"
+echo "Check above for any errors.."
+read -p "Do you want to create a flashable zip? " zip
+case $zip in
+	[Yy]* )
+	bash $KERNEL_DIR/zipping.sh
+esac
+echo "Process completed.."
---
+++ zipping.sh
@@ -0,0 +1,27 @@
+ROOT_DIR="$(dirname "$(pwd)")"
+KERNEL_DIR=$(pwd)
+ANYKERNEL_DIR=$ROOT_DIR/AnyKernel2_Pack
+BINARY_DIR=$KERNEL_DIR/arch/arm/boot
+ZIMAGE=$BINARY_DIR/zImage
+DTB=$BINARY_DIR/dt.img
+TEMP_DIR=$ROOT_DIR/temp
+KERNEL_NAME=LateAutumn
+OUTPUT_DIR=$ROOT_DIR/$KERNEL_NAME-Builds
+
+echo "Creating anykernel zip.."
+echo "Creating temporary directory.."
+mkdir $TEMP_DIR
+echo "Copying binaries.."
+cp $ZIMAGE $TEMP_DIR/zImage
+cp $DTB $TEMP_DIR/dtb
+echo "Copying anykernel files.."
+cp -r $ANYKERNEL_DIR/* $TEMP_DIR
+cd $TEMP_DIR
+read -p "Kernel version : " ver
+echo "Creating zip.."
+mkdir $OUTPUT_DIR
+ZIP_LOC=$OUTPUT_DIR/$KERNEL_NAME-$ver.zip
+zip -r9 $ZIP_LOC .
+echo "Deleting temporary directory.."
+rm -r $TEMP_DIR
+echo "Flashable zip file created.."
